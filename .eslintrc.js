a1013518a67191df9bc6753e6b53f65cb822aa5a
Takushi Nishiyama <takushi.nishiyama.0314@gmail.com>
2021-12-31 20:39:13 +0900

    eslint及びprettierの設定変更

--- .eslintrc.js
+++ .eslintrc.js
@@ -30,8 +30,18 @@ module.exports = {
     'import/extensions': ['error', 'ignorePackages', { js: 'never', jsx: 'never', ts: 'never', tsx: 'never' }],
     // Allow named export
     'import/prefer-default-export': 'off',
+    'no-mixed-spaces-and-tabs': 'error',
     'newline-before-return': 'error',
     'no-console': 'warn',
     'no-var': 'error',
+    'space-before-function-paren': 'error',
+    'space-in-parens': 'error',
+    'object-curly-spacing': 'error',
+    'semi': ['error', 'always'],
+    'semi-spacing': ['error', {'after': true, 'before': false}],
+    'semi-style': ['error', 'last'],
+    'no-extra-semi': 'error',
+    'no-unexpected-multiline': 'error',
+    'no-unreachable': 'error'
   },
 };
